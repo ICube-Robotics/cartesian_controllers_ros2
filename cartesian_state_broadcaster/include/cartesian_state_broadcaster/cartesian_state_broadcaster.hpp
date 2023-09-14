1c0b80eb97ebcf0d40b9ee6c10db122ed4c7943a
mcbed <mcbed.robotics@gmail.com>
2023-09-14 17:51:57 +0200

    fixed missing message + refactor
    fixed missing message + refactor
    
    fixed missing message + refactor

--- cartesian_state_broadcaster/include/cartesian_state_broadcaster/cartesian_state_broadcaster.hpp
+++ cartesian_state_broadcaster/include/cartesian_state_broadcaster/cartesian_state_broadcaster.hpp
@@ -15,79 +15,81 @@
 #ifndef CARTESIAN_STATE_BROADCASTER__CARTESIAN_STATE_BROADCASTER_HPP_
 #define CARTESIAN_STATE_BROADCASTER__CARTESIAN_STATE_BROADCASTER_HPP_
 
+#include <Eigen/Geometry>
+
 #include <memory>
 #include <string>
 #include <vector>
 
-#include "controller_interface/controller_interface.hpp"
+#include "realtime_tools/realtime_publisher.h"
 #include "cartesian_state_broadcaster/visibility_control.h"
-#include "cartesian_msgs/msg/cartesian_state.hpp"
+
+#include "controller_interface/controller_interface.hpp"
+#include "cartesian_control_msgs/msg/cartesian_state.hpp"
 #include "rclcpp_lifecycle/node_interfaces/lifecycle_node_interface.hpp"
 #include "rclcpp_lifecycle/state.hpp"
-#include "realtime_tools/realtime_publisher.h"
-#include <Eigen/Geometry> 
 
 // kinematics plugins
-#include "kinematics_interface/kinematics_interface_base.hpp"
+#include "kinematics_interface/kinematics_interface.hpp"
 #include "pluginlib/class_loader.hpp"
 
 namespace cartesian_state_broadcaster
 {
 using CallbackReturn = controller_interface::CallbackReturn;
-using StateMsg = cartesian_msgs::msg::CartesianState;
+using StateMsg = cartesian_control_msgs::msg::CartesianState;
 
 class CartesianStateBroadcaster : public controller_interface::ControllerInterface
 {
 public:
-    CARTESIAN_STATE_BROADCASTER_PUBLIC
-    CartesianStateBroadcaster();
+  CARTESIAN_STATE_BROADCASTER_PUBLIC
+  CartesianStateBroadcaster();
 
-    CARTESIAN_STATE_BROADCASTER_PUBLIC
-    CallbackReturn on_init() override;
+  CARTESIAN_STATE_BROADCASTER_PUBLIC
+  CallbackReturn on_init() override;
 
-    CARTESIAN_STATE_BROADCASTER_PUBLIC
-    controller_interface::InterfaceConfiguration command_interface_configuration() const override;
+  CARTESIAN_STATE_BROADCASTER_PUBLIC
+  controller_interface::InterfaceConfiguration command_interface_configuration() const override;
 
-    CARTESIAN_STATE_BROADCASTER_PUBLIC
-    controller_interface::InterfaceConfiguration state_interface_configuration() const override;
+  CARTESIAN_STATE_BROADCASTER_PUBLIC
+  controller_interface::InterfaceConfiguration state_interface_configuration() const override;
 
-    CARTESIAN_STATE_BROADCASTER_PUBLIC
-    CallbackReturn on_configure(const rclcpp_lifecycle::State & previous_state) override;
+  CARTESIAN_STATE_BROADCASTER_PUBLIC
+  CallbackReturn on_configure(const rclcpp_lifecycle::State & previous_state) override;
 
-    CARTESIAN_STATE_BROADCASTER_PUBLIC
-    CallbackReturn on_activate(const rclcpp_lifecycle::State & previous_state) override;
+  CARTESIAN_STATE_BROADCASTER_PUBLIC
+  CallbackReturn on_activate(const rclcpp_lifecycle::State & previous_state) override;
 
-    CARTESIAN_STATE_BROADCASTER_PUBLIC
-    CallbackReturn on_deactivate(const rclcpp_lifecycle::State & previous_state) override;
+  CARTESIAN_STATE_BROADCASTER_PUBLIC
+  CallbackReturn on_deactivate(const rclcpp_lifecycle::State & previous_state) override;
 
-    CARTESIAN_STATE_BROADCASTER_PUBLIC
-    controller_interface::return_type update(
-        const rclcpp::Time & time,
-        const rclcpp::Duration & period) override;
+  CARTESIAN_STATE_BROADCASTER_PUBLIC
+  controller_interface::return_type update(
+    const rclcpp::Time & time,
+    const rclcpp::Duration & period) override;
 
 protected:
-    std::string end_effector_name_;
-    std::vector<std::string> joint_names_;
-    std::vector<double> joint_positions_;
-    std::vector<double> joint_velocities_;
-    std::vector<double> cart_trafo_vec_;
-    Eigen::Matrix<double, 4, 4> cart_trafo_;
-    Eigen::Quaterniond cart_quat_;
-    std::vector<double> cart_velocities_;
+  std::string end_effector_name_;
+  std::vector<std::string> joint_names_;
+  std::vector<double> joint_positions_;
+  std::vector<double> joint_velocities_;
+  Eigen::Isometry3d cart_trafo_;
+  Eigen::Quaterniond cart_quat_;
+  std::vector<double> cart_velocities_;
 
-    // Kinematics interface plugin loader
-    std::shared_ptr<pluginlib::ClassLoader<kinematics_interface::KinematicsBaseClass>> kinematics_loader_;
-    std::unique_ptr<kinematics_interface::KinematicsBaseClass> kinematics_;
+  // Kinematics interface plugin loader
+  std::shared_ptr<pluginlib::ClassLoader<kinematics_interface::KinematicsInterface>>
+  kinematics_loader_;
+  std::unique_ptr<kinematics_interface::KinematicsInterface> kinematics_;
 
-    using StatePublisher = realtime_tools::RealtimePublisher<StateMsg>;
-    rclcpp::Publisher<StateMsg>::SharedPtr state_publisher_;
-    std::unique_ptr<StatePublisher> realtime_publisher_;
+  using StatePublisher = realtime_tools::RealtimePublisher<StateMsg>;
+  rclcpp::Publisher<StateMsg>::SharedPtr state_publisher_;
+  std::unique_ptr<StatePublisher> realtime_publisher_;
 
-    template<typename T1, typename T2>
-    void vec_to_eigen(const std::vector<T1>& data, T2 &matrix);
+  template<typename T1, typename T2>
+  void vec_to_eigen(const std::vector<T1> & data, T2 & matrix);
 
-    template<typename T1, typename T2>
-    void eigen_to_vec(const T2 &matrix, std::vector<T1>& data);
+  template<typename T1, typename T2>
+  void eigen_to_vec(const T2 & matrix, std::vector<T1> & data);
 };
 
 }  // namespace cartesian_state_broadcaster
--- cartesian_state_broadcaster/src/cartesian_state_broadcaster.cpp
+++ cartesian_state_broadcaster/src/cartesian_state_broadcaster.cpp
@@ -31,8 +31,7 @@ CallbackReturn CartesianStateBroadcaster::on_init()
     auto_declare<std::string>("end_effector_name", "tool0");
     auto_declare<std::string>("kinematics_plugin_name", "kinematics_interface_kdl/KDLKinematics");
     auto_declare<std::string>("kinematics_plugin_package", "kinematics_interface_kdl");
-  } 
-  catch (const std::exception & e) {
+  } catch (const std::exception & e) {
     fprintf(stderr, "Exception thrown during init stage with message: %s \n", e.what());
     return CallbackReturn::ERROR;
   }
@@ -43,59 +42,63 @@ CallbackReturn CartesianStateBroadcaster::on_init()
 CallbackReturn CartesianStateBroadcaster::on_configure(
   const rclcpp_lifecycle::State & /*previous_state*/)
 {
-    joint_names_ = get_node()->get_parameter("joints").as_string_array();
+  joint_names_ = get_node()->get_parameter("joints").as_string_array();
 
-    joint_positions_.resize(joint_names_.size(), std::numeric_limits<double>::quiet_NaN());
-    joint_velocities_.resize(joint_names_.size(), std::numeric_limits<double>::quiet_NaN());
-    cart_trafo_vec_.resize(16, std::numeric_limits<double>::quiet_NaN());
-    cart_velocities_.resize(7, std::numeric_limits<double>::quiet_NaN());
+  joint_positions_.resize(joint_names_.size(), std::numeric_limits<double>::quiet_NaN());
+  joint_velocities_.resize(joint_names_.size(), std::numeric_limits<double>::quiet_NaN());
+  cart_velocities_.resize(7, std::numeric_limits<double>::quiet_NaN());
 
-    end_effector_name_ = get_node()->get_parameter("end_effector_name").as_string();
-    auto kinematics_plugin_name = get_node()->get_parameter("kinematics_plugin_name").as_string();
-    auto kinematics_plugin_package = get_node()->get_parameter("kinematics_plugin_package").as_string();
+  end_effector_name_ = get_node()->get_parameter("end_effector_name").as_string();
+  auto kinematics_plugin_name = get_node()->get_parameter("kinematics_plugin_name").as_string();
+  auto kinematics_plugin_package =
+    get_node()->get_parameter("kinematics_plugin_package").as_string();
 
-      // Load the differential IK plugin
-    if (!kinematics_plugin_name.empty()) {
-      try {
-        kinematics_loader_ = std::make_shared<
-            pluginlib::ClassLoader<kinematics_interface::KinematicsBaseClass >>(
-            kinematics_plugin_package, "kinematics_interface::KinematicsBaseClass");
-        kinematics_ = std::unique_ptr<kinematics_interface::KinematicsBaseClass>(
-            kinematics_loader_->createUnmanagedInstance(kinematics_plugin_name));
-        if (!kinematics_->initialize(get_node(), end_effector_name_)) {
-            RCLCPP_ERROR(rclcpp::get_logger("CartesianStateBroadcaster"),
-                     "Exception while loading the IK plugin");
-          return CallbackReturn::ERROR;
-        }
-      }
-      catch (pluginlib::PluginlibException &ex) {
-        RCLCPP_ERROR(rclcpp::get_logger("CartesianStateBroadcaster"),
-                     "Exception while loading the IK plugin '%s': '%s'",
-                     kinematics_plugin_name.c_str(), ex.what());
+  // Load the differential IK plugin
+  if (!kinematics_plugin_name.empty()) {
+    try {
+      kinematics_loader_ = std::make_shared<
+        pluginlib::ClassLoader<kinematics_interface::KinematicsInterface>>(
+        kinematics_plugin_package, "kinematics_interface::KinematicsInterface");
+      kinematics_ = std::unique_ptr<kinematics_interface::KinematicsInterface>(
+        kinematics_loader_->createUnmanagedInstance(kinematics_plugin_name));
+      if (!kinematics_->initialize(
+          get_node()->get_node_parameters_interface(),
+          end_effector_name_))
+      {
+        RCLCPP_ERROR(
+          rclcpp::get_logger("CartesianStateBroadcaster"),
+          "Exception while loading the IK plugin");
         return CallbackReturn::ERROR;
       }
-    } 
-    else {
-      RCLCPP_ERROR(rclcpp::get_logger("CartesianStateBroadcaster"),
-                   "A differential IK plugin name was not specified in the config file.");
+    } catch (pluginlib::PluginlibException & ex) {
+      RCLCPP_ERROR(
+        rclcpp::get_logger("CartesianStateBroadcaster"),
+        "Exception while loading the IK plugin '%s': '%s'",
+        kinematics_plugin_name.c_str(), ex.what());
       return CallbackReturn::ERROR;
     }
+  } else {
+    RCLCPP_ERROR(
+      rclcpp::get_logger("CartesianStateBroadcaster"),
+      "A differential IK plugin name was not specified in the config file.");
+    return CallbackReturn::ERROR;
+  }
 
-    try {
-        // register data publisher
-        state_publisher_ = get_node()->create_publisher<StateMsg>(
-        "~/cartesian_state", rclcpp::SystemDefaultsQoS());
-        realtime_publisher_ = std::make_unique<StatePublisher>(state_publisher_);
-    } catch (const std::exception & e) {
-        fprintf(
-        stderr, "Exception thrown during publisher creation at configure stage"
-        "with message : %s \n",
-        e.what());
-        return CallbackReturn::ERROR;
-    }
+  try {
+    // register data publisher
+    state_publisher_ = get_node()->create_publisher<StateMsg>(
+      "~/cartesian_state", rclcpp::SystemDefaultsQoS());
+    realtime_publisher_ = std::make_unique<StatePublisher>(state_publisher_);
+  } catch (const std::exception & e) {
+    fprintf(
+      stderr, "Exception thrown during publisher creation at configure stage"
+      "with message : %s \n",
+      e.what());
+    return CallbackReturn::ERROR;
+  }
 
-    RCLCPP_DEBUG(get_node()->get_logger(), "configure successful");
-    return CallbackReturn::SUCCESS;
+  RCLCPP_DEBUG(get_node()->get_logger(), "configure successful");
+  return CallbackReturn::SUCCESS;
 }
 
 controller_interface::InterfaceConfiguration
@@ -109,14 +112,13 @@ CartesianStateBroadcaster::command_interface_configuration() const
 controller_interface::InterfaceConfiguration
 CartesianStateBroadcaster::state_interface_configuration() const
 {
-    controller_interface::InterfaceConfiguration state_interfaces_config;
-    state_interfaces_config.type = controller_interface::interface_configuration_type::INDIVIDUAL;
-    for (const auto & joint : joint_names_)
-    {
-        state_interfaces_config.names.push_back(joint + "/position");
-        state_interfaces_config.names.push_back(joint + "/velocity");
-    }   
-    return state_interfaces_config;
+  controller_interface::InterfaceConfiguration state_interfaces_config;
+  state_interfaces_config.type = controller_interface::interface_configuration_type::INDIVIDUAL;
+  for (const auto & joint : joint_names_) {
+    state_interfaces_config.names.push_back(joint + "/position");
+    state_interfaces_config.names.push_back(joint + "/velocity");
+  }
+  return state_interfaces_config;
 }
 
 CallbackReturn CartesianStateBroadcaster::on_activate(
@@ -133,73 +135,73 @@ CallbackReturn CartesianStateBroadcaster::on_deactivate(
 
 controller_interface::return_type CartesianStateBroadcaster::update(
   const rclcpp::Time & /* time */,
-  const rclcpp::Duration &  /* period */)
+  const rclcpp::Duration & /* period */)
 {
-    for(auto j = 0ul; j < joint_names_.size(); j++)
-    {
-        joint_positions_[j] = state_interfaces_[2*j].get_value();
-        joint_velocities_[j] = state_interfaces_[2*j+1].get_value();
-    }
-    if(!kinematics_->calculate_link_transform(joint_positions_, end_effector_name_, cart_trafo_vec_))
-    {
-        RCLCPP_ERROR(rclcpp::get_logger("CartesianStateBroadcaster"),
-                   "calculate_link_transform failed.");
-        return controller_interface::return_type::ERROR;
-    }
-    cart_trafo_.setIdentity();
-    vec_to_eigen(cart_trafo_vec_, cart_trafo_);
-    cart_quat_ = cart_trafo_.block<3,3>(0,0);
+  for (auto j = 0ul; j < joint_names_.size(); j++) {
+    joint_positions_[j] = state_interfaces_[2 * j].get_value();
+    joint_velocities_[j] = state_interfaces_[2 * j + 1].get_value();
+  }
+  if (!kinematics_->calculate_link_transform(joint_positions_, end_effector_name_, cart_trafo_)) {
+    RCLCPP_ERROR(
+      rclcpp::get_logger("CartesianStateBroadcaster"),
+      "calculate_link_transform failed.");
+    return controller_interface::return_type::ERROR;
+  }
 
-    if(!kinematics_->convert_joint_deltas_to_cartesian_deltas(joint_positions_, joint_velocities_, 
-                                                            end_effector_name_, cart_velocities_))
-    {
-        RCLCPP_ERROR(rclcpp::get_logger("CartesianStateBroadcaster"),
-                   "convert_joint_deltas_to_cartesian_deltas failed.");
-        return controller_interface::return_type::ERROR;
-    }
+  if (!kinematics_->convert_joint_deltas_to_cartesian_deltas(
+      joint_positions_, joint_velocities_,
+      end_effector_name_, cart_velocities_))
+  {
+    RCLCPP_ERROR(
+      rclcpp::get_logger("CartesianStateBroadcaster"),
+      "convert_joint_deltas_to_cartesian_deltas failed.");
+    return controller_interface::return_type::ERROR;
+  }
 
-    if (realtime_publisher_ && realtime_publisher_->trylock()) {
-        auto & cart_state_msg = realtime_publisher_->msg_;
-        cart_state_msg.header.stamp = get_node()->now();
-        cart_state_msg.link_name = end_effector_name_;
+  if (realtime_publisher_ && realtime_publisher_->trylock()) {
+    auto & cart_state_msg = realtime_publisher_->msg_;
+    cart_state_msg.header.stamp = get_node()->now();
+    cart_state_msg.link_name = end_effector_name_;
 
-        cart_state_msg.pose.position.x = cart_trafo_(0,3);
-        cart_state_msg.pose.position.y = cart_trafo_(1,3);
-        cart_state_msg.pose.position.z = cart_trafo_(2,3);
-        cart_state_msg.pose.orientation.x = cart_quat_.x();
-        cart_state_msg.pose.orientation.y = cart_quat_.y();
-        cart_state_msg.pose.orientation.z = cart_quat_.z();
-        cart_state_msg.pose.orientation.w = cart_quat_.w();
+    cart_state_msg.pose.position.x = cart_trafo_(0, 3);
+    cart_state_msg.pose.position.y = cart_trafo_(1, 3);
+    cart_state_msg.pose.position.z = cart_trafo_(2, 3);
+    cart_state_msg.pose.orientation.x = cart_quat_.x();
+    cart_state_msg.pose.orientation.y = cart_quat_.y();
+    cart_state_msg.pose.orientation.z = cart_quat_.z();
+    cart_state_msg.pose.orientation.w = cart_quat_.w();
 
-        cart_state_msg.twist.linear.x = cart_velocities_[0];
-        cart_state_msg.twist.linear.y = cart_velocities_[1];
-        cart_state_msg.twist.linear.z = cart_velocities_[2];
-        cart_state_msg.twist.angular.x = cart_velocities_[3];
-        cart_state_msg.twist.angular.y = cart_velocities_[4];
-        cart_state_msg.twist.angular.z = cart_velocities_[5];
+    cart_state_msg.twist.linear.x = cart_velocities_[0];
+    cart_state_msg.twist.linear.y = cart_velocities_[1];
+    cart_state_msg.twist.linear.z = cart_velocities_[2];
+    cart_state_msg.twist.angular.x = cart_velocities_[3];
+    cart_state_msg.twist.angular.y = cart_velocities_[4];
+    cart_state_msg.twist.angular.z = cart_velocities_[5];
 
-        realtime_publisher_->unlockAndPublish();
-    }
+    realtime_publisher_->unlockAndPublish();
+  }
 
-    return controller_interface::return_type::OK;
+  return controller_interface::return_type::OK;
 }
 
 template<typename T1, typename T2>
-void CartesianStateBroadcaster::vec_to_eigen(const std::vector<T1> &data, T2 &matrix) {
-    for (auto col = 0; col < matrix.cols(); col++) {
-        for (auto row = 0; row < matrix.rows(); row++) {
-            matrix(row, col) = data[row + col * matrix.rows()];
-        }
+void CartesianStateBroadcaster::vec_to_eigen(const std::vector<T1> & data, T2 & matrix)
+{
+  for (auto col = 0; col < matrix.cols(); col++) {
+    for (auto row = 0; row < matrix.rows(); row++) {
+      matrix(row, col) = data[row + col * matrix.rows()];
     }
+  }
 }
 
 template<typename T1, typename T2>
-void CartesianStateBroadcaster::eigen_to_vec(const T2 &matrix, std::vector<T1> &data) {
-    for (auto col = 0; col < matrix.cols(); col++) {
-        for (auto row = 0; row < matrix.rows(); row++) {
-            data[row + col * matrix.rows()] = matrix(row, col);
-        }
+void CartesianStateBroadcaster::eigen_to_vec(const T2 & matrix, std::vector<T1> & data)
+{
+  for (auto col = 0; col < matrix.cols(); col++) {
+    for (auto row = 0; row < matrix.rows(); row++) {
+      data[row + col * matrix.rows()] = matrix(row, col);
     }
+  }
 }
 
 }  // namespace cartesian_state_broadcaster
