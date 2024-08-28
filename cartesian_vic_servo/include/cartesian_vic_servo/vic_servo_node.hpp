0215c08ccf41673aa88bc36ed8356a13002b5aa2
Thibault Poignonec <tpoignonec@unistra.fr>
2024-08-28 17:04:49 +0200

    add joint state subscriber

--- cartesian_vic_servo/include/cartesian_vic_servo/vic_servo_node.hpp
+++ cartesian_vic_servo/include/cartesian_vic_servo/vic_servo_node.hpp
@@ -1,13 +1,31 @@
-// TODO: add copyright
+// Copyright 2024 ICUBE Laboratory, University of Strasbourg
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 
 #ifndef CARTESIAN_VIC_SERVO__VIC_SERVO_NODE_HPP_
 #define CARTESIAN_VIC_SERVO__VIC_SERVO_NODE_HPP_
 
-#include "rclcpp/rclcpp.hpp"
+#include <memory>
+#include <string>
+#include <vector>
 
-#include "geometry_msgs/msg/Twist.hpp"
-#include "geometry_msgs/msg/Wrench.hpp"
-#include "sensor_msgs/msg/JointState.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "realtime_tools/realtime_buffer.h"
+
+#include "geometry_msgs/msg/twist.hpp"
+#include "geometry_msgs/msg/wrench.hpp"
+#include "sensor_msgs/msg/joint_state.hpp"
 
 #include "cartesian_control_msgs/msg/compliant_frame_trajectory.hpp"
 #include "cartesian_control_msgs/msg/vic_controller_state.hpp"
@@ -15,59 +33,57 @@
 namespace cartesian_vic_servo
 {
 
-class CartesianVicServo : rclcpp::Node
+class CartesianVicServo : public rclcpp::Node
 {
 public:
-    explicit CartesianVicServo(std::string node_name = "cartesian_vic_servo_node");
-    ~CartesianVicServo = default;
+  explicit CartesianVicServo(std::string node_name = "cartesian_vic_servo_node");
+  ~CartesianVicServo() = default;
 
-    bool init();
+  bool init();
 
-    bool start();
+  bool start();
 
-    bool stop();
+  bool stop();
 
-    bool update();
-
-    sensor_msgs::msg::JointState jointState;
-    geometry_msgs::msg::Wrench wrench;
-    cartesian_control_msgs::msg::ComplianceFrameTrajectory vicTrajectory;
-
-
+  bool update();
 
 protected:
     // void callback_new_joint_state_msg(...);
     // + wrench (StampedWrench ou Wrench)
     // + vic ref (voir cartesian_control_msgs)
 
-    bool check_timeout_state();
-
     // bool send_servo_command(const Eigen::Vector<double, 6> & twist_cmd);
 
 protected:
-    /// @brief True if init() called sucessfully
-    bool is_initialized_ = false;
+    /// @brief True if init() called successfully
+  bool is_initialized_ = false;
 
     /// @brief True when ready to compute VIC commands
-    bool is_ready_ = false;
+  bool is_ready_ = false;
 
-    // Subscribers
-    rclcpp::Subscription<sensor_msgs::msg::JointState>::SharedPtr subscriber_joint_state_;
-    rclcpp::Subscription<geometry_msgs::msg::Wrench>::SharedPtr subscriber_wrench_;
-    rclcpp::Subscription<cartesian_control_msgs::msg::ComplianceFrameTrajectory>::SharedPtr subscriber_vic_trajectory_;
+    // Subscriber joint state
+  rclcpp::Subscription<sensor_msgs::msg::JointState>::SharedPtr
+    joint_state_subscriber_;
+  realtime_tools::RealtimeBuffer<std::shared_ptr<
+      sensor_msgs::msg::JointState>> rt_buffer_joint_state_;
+  std::shared_ptr<sensor_msgs::msg::JointState> joint_state_msg_ptr_;
+
+  rclcpp::Subscription<geometry_msgs::msg::Wrench>::SharedPtr
+    subscriber_wrench_;
+  rclcpp::Subscription<cartesian_control_msgs::msg::CompliantFrameTrajectory>::SharedPtr
+    subscriber_vic_trajectory_;
 
     // PUblishers
-    rclcpp::PUblisher<cartesian_control_msgs::msg::VicControllerState>::SharedPtr publisher_vic_state_;
-    rclcpp::PUblisher<geometry_msgs::msg::Twist>::SharedPtr publisher_twist_;
+  rclcpp::Publisher<cartesian_control_msgs::msg::VicControllerState>::SharedPtr
+    publisher_vic_state_;
+  rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr
+    publisher_twist_;
 
-    void jointState_callback(sensor_msgs::msg::JointState msg);
+  bool get_joint_state(
+    sensor_msgs::msg::JointState & msg,
+    double timeout = 0.01 /*seconds*/);
+};
 
-
-}
-
-
-
-
-}
+}  // namespace cartesian_vic_servo
 
 #endif  // CARTESIAN_VIC_SERVO__VIC_SERVO_NODE_HPP_
--- cartesian_vic_servo/src/vic_servo_node.cpp
+++ cartesian_vic_servo/src/vic_servo_node.cpp
@@ -1,31 +1,121 @@
-#include <cstdio>
+// Copyright 2024 ICUBE Laboratory, University of Strasbourg
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-#include "rclcpp/rclcpp.hpp"
-#include "geometry_msgs/msg/Twist.hpp"
-#include "geometry_msgs/msg/Wrench.hpp"
-#include "sensor_msgs/msg/JointState.hpp"
-#include "cartesian_control_msgs/msg/compliant_frame_trajectory.hpp"
-#include "cartesian_control_msgs/msg/vic_controller_state.hpp"
+#include "cartesian_vic_servo/vic_servo_node.hpp"
 
+// #include "rcutils/logging_macros.h"
+#include "rclcpp/logging.hpp"
 
+namespace cartesian_vic_servo
+{
 
+CartesianVicServo::CartesianVicServo(std::string node_name)
+: Node(node_name)
+{
+  // Nothing to do
+}
 bool CartesianVicServo::init()
 {
-  // Subscribers
-  subscriber_joint_state_ = this->create_subscription<sensor_msgs::msg::JointState>("joint_state_topic", 1, jointState_callback);
-  subscriber_wrench_ = this->create_subscription<geometry_msgs::msg::Wrench>();
-  subscriber_vic_trajectory_ = this->create_subscription<cartesian_control_msgs::msg::ComplianceFrameTrajectory>();
+  // Setup joint state subscribers
+  auto joint_state_callback =
+    [this](const std::shared_ptr<sensor_msgs::msg::JointState> msg)
+    {rt_buffer_joint_state_.writeFromNonRT(msg);};
+  joint_state_subscriber_ = \
+    this->create_subscription<sensor_msgs::msg::JointState>("joint_states", 1,
+      joint_state_callback);
 
+  /*
+  subscriber_wrench_ = \
+    this->create_subscription<geometry_msgs::msg::Wrench>();
+  subscriber_vic_trajectory_ = \
+    this->create_subscription<cartesian_control_msgs::msg::CompliantFrameTrajectory>();
+  */
 
   // Publishers
+  /*
   publisher_vic_state_ = this->create_publisher<cartesian_control_msgs::msg::VicControllerState>("input_vic_state", 1);
   publisher_twist_ = this->create_publisher<geometry_msgs::msg::Twist>("/twist_command", 1);
+  */
 
+  /*
+  state_publisher_ =
+    std::make_unique<realtime_tools::RealtimePublisher<ControllerStateMsg>>(s_publisher_);
+
+  // How to use:
+  state_publisher_->lock();
+  // state_publisher_->msg_ = vic_->get_controller_state();
+  state_publisher_->unlock();
+  */
   return true;
 }
 
-auto jointState_callback(sensor_msgs::msg::JointState msg)
+bool CartesianVicServo::start()
 {
-  this->jointState = msg;
+  return false;
+}
 
+bool CartesianVicServo::stop()
+{
+  return false;
+}
+
+bool CartesianVicServo::update()
+{
+  return false;
+}
+
+// -----------------------------------------------------
+//  Subscribers callbacks
+// -----------------------------------------------------
+
+bool CartesianVicServo::get_joint_state(
+  sensor_msgs::msg::JointState & joint_state_msg,
+  double timeout /*seconds*/)
+{
+  // Get msg from RT buffer
+  joint_state_msg_ptr_ = \
+    *rt_buffer_joint_state_.readFromRT();
+  if (joint_state_msg_ptr_.get()) {
+    joint_state_msg = *joint_state_msg_ptr_.get();
+  } else {
+    RCLCPP_ERROR(
+      this->get_logger(),
+      "Failed to get joint state message!");
+    return false;
+  }
+
+  // Check timeout
+  double delay = (this->now() - joint_state_msg.header.stamp).nanoseconds();
+  if (delay > timeout * 1e9) {
+    RCLCPP_ERROR(
+      this->get_logger(),
+      "Timeout on joint state message!");
+    return false;
+  }
+
+  return true;
+}
+// -----------------------------------------------------
+// Utils
+// -----------------------------------------------------
+
+}  // namespace cartesian_vic_servo
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<cartesian_vic_servo::CartesianVicServo>());
+  rclcpp::shutdown();
+  return 0;
 }
