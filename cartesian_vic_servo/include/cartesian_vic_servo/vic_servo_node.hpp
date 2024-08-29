36de04dc5170607bb07e86e5dc3195900c278383
Thibault Poignonec <tpoignonec@unistra.fr>
2024-08-29 16:50:48 +0200

    add VIC rule to cartesian_vic_servo node

--- cartesian_vic_servo/include/cartesian_vic_servo/vic_servo_node.hpp
+++ cartesian_vic_servo/include/cartesian_vic_servo/vic_servo_node.hpp
@@ -28,6 +28,9 @@
 #include "geometry_msgs/msg/wrench_stamped.hpp"
 #include "sensor_msgs/msg/joint_state.hpp"
 
+#include "cartesian_vic_controller/measurement_data.hpp"
+#include "cartesian_vic_controller/cartesian_vic_rule.hpp"
+
 #include "cartesian_control_msgs/msg/compliant_frame_trajectory.hpp"
 #include "cartesian_control_msgs/msg/vic_controller_state.hpp"
 
@@ -62,7 +65,31 @@ protected:
     /// @brief True when ready to compute VIC commands
   bool is_ready_ = false;
 
-    // Subscriber joint state
+  // number of robot controlled joints
+  size_t num_joints_ = 0;
+
+  // Vic rule loader
+  std::shared_ptr<pluginlib::ClassLoader<cartesian_vic_controller::CartesianVicRule>>
+  vic_loader_;
+
+  // Vic rule
+  std::unique_ptr<cartesian_vic_controller::CartesianVicRule> vic_;
+  bool is_vic_initialized_ = false;
+
+  // vic parameters
+  std::shared_ptr<cartesian_vic_controller::ParamListener> parameter_handler_;
+
+  // measurement data used by the vic controller
+  //   - joint state: current joint readings from the hardware
+  //   - measured wrench: values read from the ft sensor (optional)
+  //   - external torques: values read from the external torque sensor (optional)
+  cartesian_vic_controller::MeasurementData measurement_data_{0};
+
+  sensor_msgs::msg::JointState joint_state_msg_;
+  geometry_msgs::msg::WrenchStamped wrench_msg_;
+  cartesian_control_msgs::msg::CompliantFrameTrajectory vic_trajectory_msg_;
+
+  // Subscriber joint state
   rclcpp::Subscription<sensor_msgs::msg::JointState>::SharedPtr
     joint_state_subscriber_;
   realtime_tools::RealtimeBuffer<std::shared_ptr<
@@ -81,30 +108,28 @@ protected:
       cartesian_control_msgs::msg::CompliantFrameTrajectory>> rt_buffer_vic_trajectory_;
   std::shared_ptr<cartesian_control_msgs::msg::CompliantFrameTrajectory> vic_trajectory_msg_ptr_;
 
-    // PUblishers
-  //rclcpp::Publisher<cartesian_control_msgs::msg::VicControllerState>::SharedPtr publisher_vic_state_;
-  std::shared_ptr<rclcpp::Publisher<cartesian_control_msgs::msg::VicControllerState>> publisher_vic_state_;
-  std::shared_ptr<realtime_tools::RealtimePublisher<cartesian_control_msgs::msg::VicControllerState>> rt_publisher_vic_state;
+  // Publishers
+  std::shared_ptr<rclcpp::Publisher<cartesian_control_msgs::msg::VicControllerState>>
+  publisher_vic_state_;
+  std::shared_ptr<realtime_tools::RealtimePublisher<
+      cartesian_control_msgs::msg::VicControllerState>> rt_publisher_vic_state;
 
-  //rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_twist_;
   std::shared_ptr<rclcpp::Publisher<geometry_msgs::msg::Twist>> publisher_twist_;
   std::shared_ptr<realtime_tools::RealtimePublisher<geometry_msgs::msg::Twist>> rt_publisher_twist_;
+
+  bool update_measurement_data();
 
   bool get_joint_state(
     sensor_msgs::msg::JointState & msg,
     double timeout = 0.01 /*seconds*/);
 
-  bool get_vic_trajectory(
-    cartesian_control_msgs::msg::CompliantFrameTrajectory & msg,
-    double timeout = 0.01 /*seconds*/);
-
-
   bool get_wrench(
     geometry_msgs::msg::WrenchStamped & msg,
     double timeout = 0.01 /*seconds*/);
 
-
-
+  bool get_vic_trajectory(
+    cartesian_control_msgs::msg::CompliantFrameTrajectory & msg,
+    double timeout = 0.01 /*seconds*/);
 };
 
 }  // namespace cartesian_vic_servo
--- cartesian_vic_servo/src/vic_servo_node.cpp
+++ cartesian_vic_servo/src/vic_servo_node.cpp
@@ -36,41 +36,73 @@ bool CartesianVicServo::init()
     this->create_subscription<sensor_msgs::msg::JointState>("joint_states", 1,
       joint_state_callback);
 
-  
+
   // Setup wrench  subscriber
   auto wrench_callback =
     [this](const std::shared_ptr<geometry_msgs::msg::WrenchStamped> msg)
     {rt_buffer_wrench_.writeFromNonRT(msg);};
   subscriber_wrench_ = \
     this->create_subscription<geometry_msgs::msg::WrenchStamped>("wrench", 1, wrench_callback);
-  
+
 
   // Setup vic trajectory subscriber
   auto vic_trajectory_callback =
     [this](const std::shared_ptr<cartesian_control_msgs::msg::CompliantFrameTrajectory> msg)
     {rt_buffer_vic_trajectory_.writeFromNonRT(msg);};
   subscriber_vic_trajectory_ = \
-    this->create_subscription<cartesian_control_msgs::msg::CompliantFrameTrajectory>("vic_trajectory", 1, vic_trajectory_callback);
-  
+    this->create_subscription<cartesian_control_msgs::msg::CompliantFrameTrajectory>(
+      "vic_trajectory", 1, vic_trajectory_callback);
 
   // Publishers
-  
-  publisher_vic_state_ = this->create_publisher<cartesian_control_msgs::msg::VicControllerState>("input_vic_state", 1);
+  publisher_vic_state_ =
+    this->create_publisher<cartesian_control_msgs::msg::VicControllerState>("input_vic_state", 1);
   publisher_twist_ = this->create_publisher<geometry_msgs::msg::Twist>("/twist_command", 1);
 
   // Realtime publisher
   rt_publisher_vic_state =
-    std::make_unique<realtime_tools::RealtimePublisher<cartesian_control_msgs::msg::VicControllerState>>(publisher_vic_state_);
+    std::make_unique<realtime_tools::RealtimePublisher<
+        cartesian_control_msgs::msg::VicControllerState>>(publisher_vic_state_);
 
-  rt_publisher_twist_ =
-    std::make_unique<realtime_tools::RealtimePublisher<geometry_msgs::msg::Twist>>(publisher_twist_);
+  rt_publisher_twist_ = std::make_unique<realtime_tools::RealtimePublisher<
+        geometry_msgs::msg::Twist>>(publisher_twist_);
 
-  /*
-  // How to use:
-  state_publisher_->lock();
-  // state_publisher_->msg_ = vic_->get_controller_state();
-  state_publisher_->unlock();
-  */
+  // initialize controller config
+  try {
+    parameter_handler_ =
+      std::make_shared<cartesian_vic_controller::ParamListener>(shared_from_this());
+    cartesian_vic_controller::Params parameters = parameter_handler_->get_params();
+    // number of joints in controllers is fixed after initialization
+    num_joints_ = parameters.joints.size();
+    RCLCPP_INFO(get_logger(), "Configuring controller with %li joints", num_joints_);
+    // allocate dynamic memory
+    measurement_data_ = cartesian_vic_controller::MeasurementData(parameters.joints.size());
+    if (!parameters.vic.plugin_name.empty() &&
+      !parameters.vic.plugin_package.empty())
+    {
+      vic_loader_ =
+        std::make_shared<pluginlib::ClassLoader<
+            cartesian_vic_controller::CartesianVicRule>>(
+        parameters.vic.plugin_package,
+        "cartesian_vic_controller::CartesianVicRule"
+            );
+      vic_ = std::unique_ptr<cartesian_vic_controller::CartesianVicRule>(
+        vic_loader_->createUnmanagedInstance(parameters.vic.plugin_name));
+    } else {
+      RCLCPP_ERROR(
+        get_logger(),
+        "Please provide 'vic.plugin_package' and 'vic.plugin_name' parameters!");
+      return false;
+    }
+    // Initialize vic rule plugin
+    if (vic_->init(parameter_handler_) == controller_interface::return_type::ERROR) {
+      return false;
+    }
+  } catch (const std::exception & e) {
+    RCLCPP_ERROR(
+      get_logger(), "Exception thrown during configure stage with message: %s \n",
+      e.what());
+    return false;
+  }
   return true;
 }
 
@@ -86,7 +118,91 @@ bool CartesianVicServo::stop()
 
 bool CartesianVicServo::update()
 {
-  return false;
+  // TODO(dmeckes): move to init() (+ make those node parameters?)
+  double timeout = 0.01;
+  double Ts = 0.005;  // 200 Hz
+
+  // -----------------------
+  // Prepare data for vic
+  // -----------------------
+  bool is_state_valid = true;
+  // Get joint state
+  is_state_valid &= get_joint_state(joint_state_msg_, timeout);
+  // Get wrench
+  is_state_valid &= get_wrench(wrench_msg_, timeout);
+  // Get vic trajectory
+  is_state_valid &= get_vic_trajectory(vic_trajectory_msg_, timeout);
+
+  if (is_state_valid) {
+    is_state_valid &= update_measurement_data();
+  } else {
+    // Invalid state detected!
+    RCLCPP_ERROR(
+      this->get_logger(),
+      "Failed to get state message!");
+  }
+  // -----------------------
+  // Run vic
+  // -----------------------
+  geometry_msgs::msg::Twist twist_cmd;
+  // twist_cmd.header.stamp = this->now();
+  // twist_cmd.header.frame_id = base_link_;
+  bool all_ok = is_state_valid;
+  if (!is_vic_initialized_ && !is_state_valid) {
+    // Exit and wait for valid data...
+    all_ok = false;
+  } else if (!is_vic_initialized_ && is_state_valid) {
+    // Init current desired pose from current joint position
+    is_vic_initialized_ = (vic_->init_reference_frame_trajectory(
+        measurement_data_.get_joint_state()) == controller_interface::return_type::OK);
+    if(!is_vic_initialized_) {
+      all_ok = false;
+      RCLCPP_ERROR(
+      get_logger(),
+      "Failed to initialize the reference compliance frame trajectory.");
+    }
+  }
+
+  // Send zero twist if not all OK
+  if(!all_ok) {
+    // TODO(dmeckes): send twist_cmd = 0
+    return false;
+  }
+
+  // Otherwise, proceed to control logic
+  // 1) Update vic
+  rclcpp::Duration period = rclcpp::Duration::from_seconds(Ts);
+  auto ret_vic = vic_->update_input_data(
+      period,
+      measurement_data_
+  );
+  if (ret_vic != controller_interface::return_type::OK) {
+    RCLCPP_ERROR(
+      this->get_logger(),
+      "Failed to update VIC!");
+    all_ok = false;
+  }
+  ret_vic = vic_->compute_controls(period, twist_cmd);
+  if (ret_vic != controller_interface::return_type::OK) {
+    RCLCPP_ERROR(
+      this->get_logger(),
+      "Failed to get commands from VIC rule!");
+    all_ok = false;
+  }
+
+  // Send zero twist if not all OK
+  if(!all_ok) {
+    // TODO(dmeckes): send twist_cmd = 0
+    return false;
+  }
+
+  // 2) Send twist command
+
+  // TODO(dmeckes): send the twist cmd to moveit servo
+
+  // 3) Send controller VIC state
+
+  // TODO(dmeckes): send the state msg
 }
 
 // -----------------------------------------------------
@@ -146,7 +262,6 @@ bool CartesianVicServo::get_wrench(
     return false;
   }
   return true;
-
 }
 
 bool CartesianVicServo::get_vic_trajectory(
@@ -181,6 +296,34 @@ bool CartesianVicServo::get_vic_trajectory(
 // -----------------------------------------------------
 // Utils
 // -----------------------------------------------------
+
+bool CartesianVicServo::update_measurement_data()
+{
+  measurement_data_.reset_data_availability();
+  bool all_ok = true;
+
+  auto & state_current = measurement_data_.joint_state;
+  bool state_has_nan = false;
+  for (size_t joint_ind = 0; joint_ind < num_joints_; ++joint_ind) {
+    state_current.positions[joint_ind] = \
+      joint_state_msg_.position[joint_ind];
+    state_current.velocities[joint_ind] = \
+      joint_state_msg_.velocity[joint_ind];
+    // Detect NaNs
+    state_has_nan &= std::isnan(state_current.positions[joint_ind]);
+    state_has_nan &= std::isnan(state_current.velocities[joint_ind]);
+  }
+  if (state_has_nan) {
+    all_ok = false;
+    RCLCPP_ERROR(
+      this->get_logger(),
+      "NaN detected in state message!");
+  }
+  // Update ft sensor wrench
+  all_ok &= measurement_data_.update_ft_sensor_wrench(wrench_msg_.wrench);
+
+  return all_ok;
+}
 
 }  // namespace cartesian_vic_servo
 
