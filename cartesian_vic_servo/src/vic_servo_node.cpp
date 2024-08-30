7c22590a79739baf4225ead8a229b523f51f462c
dylmec <d.meckes@unistra.fr>
2024-08-30 17:11:44 +0200

    add stop timer in stop()

--- cartesian_vic_servo/src/vic_servo_node.cpp
+++ cartesian_vic_servo/src/vic_servo_node.cpp
@@ -176,13 +176,21 @@ bool CartesianVicServo::start()
   //timer
   Ts_ = 5.0; //5 milliseconds -> 200Hz (find a way to read from launch file)
   timer_ = this->create_wall_timer(std::chrono::milliseconds(500), std::bind(&CartesianVicServo::CartesianVicServo::update, this));
-  
+  RCLCPP_ERROR(
+        get_logger(),
+        "Timer: started!");
+
   return false;
 }
 
 bool CartesianVicServo::stop()
 {
   // TODO(dmeckes): stop timer
+  timer_.reset();
+  RCLCPP_ERROR(
+        get_logger(),
+        "Timer: stopped!");
+
 
   // TODO(dmeckes): send twist = 0 to moveit servo
   null_twist_->header.stamp = this->now(); //add time
