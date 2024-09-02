c18dd9e5612243a77021a98fd463db3059de4152
dylmec <d.meckes@unistra.fr>
2024-09-02 15:58:32 +0200

    include relevent .h for moveit

--- cartesian_vic_servo/include/cartesian_vic_servo/vic_servo_node.hpp
+++ cartesian_vic_servo/include/cartesian_vic_servo/vic_servo_node.hpp
@@ -28,6 +28,9 @@
 #include "geometry_msgs/msg/wrench_stamped.hpp"
 #include "sensor_msgs/msg/joint_state.hpp"
 
+#include "moveit/move_group_interface/move_group_interface.h"
+#include "moveit/planning_scene_interface/planning_scene_interface.h"
+
 #include "cartesian_vic_controller/measurement_data.hpp"
 #include "cartesian_vic_controller/cartesian_vic_rule.hpp"
 
