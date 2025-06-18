2ad8a0a6de1a101528a676810d8510ce113b1c21
Thibault <thibault.poignonec@gmail.com>
2025-06-18 11:39:37 +0200

    cleanup ext FT sensor

--- cartesian_vic_controller/include/cartesian_vic_controller/external_torque_sensor.hpp
+++ cartesian_vic_controller/include/cartesian_vic_controller/external_torque_sensor.hpp
@@ -21,6 +21,8 @@
 #include <string>
 #include <vector>
 
+#include "rclcpp/logging.hpp"
+
 #include "controller_interface/helpers.hpp"
 #include "hardware_interface/loaned_state_interface.hpp"
 
@@ -31,6 +33,7 @@ class ExternalTorqueSensor
 {
 public:
   explicit ExternalTorqueSensor(const std::string & name, size_t num_joints)
+  : logger_(rclcpp::get_logger("cartesian_vic_controller.external_torque_sensor"))
   {
     name_ = name;
     num_joints_ = num_joints;
@@ -51,41 +54,9 @@ public:
     std::fill(torques_.begin(), torques_.end(), std::numeric_limits<double>::quiet_NaN());
   }
 
-  ~ExternalTorqueSensor() = default;
-
-  bool assign_loaned_state_interfaces(
-    std::vector<hardware_interface::LoanedStateInterface> & state_interfaces)
-  {
-    return controller_interface::get_ordered_interfaces(
-      state_interfaces, interface_names_, "", state_interfaces_);
-  }
-
-  void release_interfaces() {state_interfaces_.clear();}
-
-  bool get_values(std::vector<double> & values) const
-  {
-    // check we have sufficient memory
-    if (values.capacity() != state_interfaces_.size()) {
-      return false;
-    }
-    // insert all the values
-    for (size_t i = 0; i < state_interfaces_.size(); ++i) {
-      values.emplace_back(state_interfaces_[i].get().get_value());
-    }
-    return true;
-  }
-
-  std::vector<std::string> get_state_interface_names()
-  {
-    if (interface_names_.empty()) {
-      for (auto i = 0u; i < interface_names_.capacity(); ++i) {
-        interface_names_.emplace_back(name_ + "/" + std::to_string(i + 1));
-      }
-    }
-    return interface_names_;
-  }
 
   explicit ExternalTorqueSensor(const std::vector<std::string> & interfaces_external_torque)
+  : logger_(rclcpp::get_logger("cartesian_vic_controller.external_torque_sensor"))
   {
     name_ = "";
     num_joints_ = interfaces_external_torque.size();
@@ -106,12 +77,62 @@ public:
     std::fill(torques_.begin(), torques_.end(), std::numeric_limits<double>::quiet_NaN());
   }
 
+  ~ExternalTorqueSensor() = default;
+
+  bool assign_loaned_state_interfaces(
+    std::vector<hardware_interface::LoanedStateInterface> & state_interfaces)
+  {
+    return controller_interface::get_ordered_interfaces(
+      state_interfaces, interface_names_, "", state_interfaces_);
+  }
+
+  void release_interfaces() {state_interfaces_.clear();}
+
+  bool get_values(std::vector<double> & values) const
+  {
+    bool all_ok = true;
+    // check we have sufficient memory
+    if (values.capacity() != state_interfaces_.size()) {
+      return false;
+    }
+    // insert all the values
+    for (size_t i = 0; i < state_interfaces_.size(); ++i) {
+      auto value_i = state_interfaces_[i].get().get_optional<double>();
+      if (!value_i.has_value()) {
+        RCLCPP_WARN(logger_, "Failed to read value for interface %zu", i);
+        all_ok = false;
+        values.emplace_back(std::numeric_limits<double>::quiet_NaN());
+        continue;
+      }
+      // If the value is available, insert it
+      values.emplace_back(value_i.value());
+    }
+    return all_ok;
+  }
+
+  std::vector<std::string> get_state_interface_names()
+  {
+    if (interface_names_.empty()) {
+      for (auto i = 0u; i < interface_names_.capacity(); ++i) {
+        interface_names_.emplace_back(name_ + "/" + std::to_string(i + 1));
+      }
+    }
+    return interface_names_;
+  }
+
   std::vector<double> get_torques()
   {
     auto torque_interface_counter = 0;
     for (size_t i = 0; i < num_joints_; ++i) {
       if (existing_axes_[i]) {
-        torques_[i] = state_interfaces_[torque_interface_counter].get().get_value();
+        auto torque_i = state_interfaces_[torque_interface_counter].get().get_optional<double>();
+        if (torque_i.has_value()) {
+          torques_[i] = torque_i.value();
+        } else {
+          // If the value is not available, set it to NaN
+          torques_[i] = std::numeric_limits<double>::quiet_NaN();
+          RCLCPP_WARN(logger_, "Failed to read torque for joint %zu", i);
+        }
         ++torque_interface_counter;
       }
     }
@@ -119,6 +140,8 @@ public:
   }
 
 protected:
+  rclcpp::Logger logger_;
+
   std::string name_;
   size_t num_joints_;
   std::vector<std::string> interface_names_;
